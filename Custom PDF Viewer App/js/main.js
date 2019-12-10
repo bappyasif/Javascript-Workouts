d929db34f18bebb38bd1ca743bc142755002501d
BaPpY <bappyasif@hotmail.com>
2019-12-11 02:16:17 +0600

    Added To Previous Commit With Rest Of This Implementation Of PDF Viewer App Using JavaScript.

--- Custom PDF Viewer App/js/main.js
+++ Custom PDF Viewer App/js/main.js
@@ -1,11 +1,12 @@
 const url = '../docs/sample.pdf';
+//const url = '../docs/sample1.pdf';
 
 let pdfDoc = null,
  pageNum = 1,
  pageIsRendering = false,
  pageNumIsPending = null;
 
-const scale = 1.5,
+const scale = 1.1,
  canvas = document.querySelector("#pdf-render"),
  ctx = canvas.getContext("2d");
 
@@ -37,6 +38,33 @@ const renderPage = num => {
         // Output Page
         document.querySelector("#page-num").textContent = num;
     });
+};
+
+// Check For Pages Rendring
+const queryRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+}
+
+// Navigate To Previous Page
+const showPrevPage = () => {
+    if(pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queryRenderPage(pageNum);
+}
+
+// Navigate To Next Page
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queryRenderPage(pageNum);
 }
 
 // Get Document
@@ -47,4 +75,18 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
     document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
     renderPage(pageNum);
+})
+    .catch(error => {
+        // Display Error
+        const div = document.createElement("div");
+        div.className = "error";
+        div.appendChild(document.createTextNode(error.message));
+        //document.querySelector("body").replaceWith(div, canvas);
+        document.querySelector("body").insertBefore(div, canvas);
+        // Removal Of Top Bar 
+        document.querySelector(".top-bar").style.display = "null";
 });
+
+// Button Events
+document.querySelector("#prev-page").addEventListener("click", showPrevPage);
+document.querySelector("#next-page").addEventListener("click", showNextPage);
