908193f5793b5e3947f3f6a2672c2021f944ddc4
BaPpY <bappyasif@hotmail.com>
2019-12-15 02:27:02 +0600

    Added To Previous Commit With Rest Of This Bookmark Save App With Local Storage Capabilty For Data Persistency.

--- Bookmarker App/js/main.js
+++ Bookmarker App/js/main.js
@@ -13,6 +13,10 @@ function saveBookmark(event) {
     console.log(siteUrl);
     //console.log("It Worrks!!");
 
+    if(!validationBookmarks(siteName, siteUrl)) {
+        return false;
+    }
+
     // Bookmarked Objct
     var bookmarkSaved = {
         name: siteName,
@@ -46,8 +50,33 @@ function saveBookmark(event) {
 
         // Save To Local Storage
         localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+        // Clear Form After Entry
+        document.getElementById("myForm").reset();
+
+        // Re-fetch All Saved Bookmarks
+        fetchBookmarks();
     }
 
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+    //console.log(url);
+    // Get Bookmarks From LocalStorage
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    // Loop Through Bookmarks Elements
+    for(var i = 0; i<bookmarks.length; i++) {
+        if(bookmarks[i].url == url) {
+            // Remove From This Array
+            bookmarks.splice(i,1);
+        }
+    }
+     // Re-Set Back To LocalStorage
+     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+     // Re-fetch Bookmarks To Regenerate Updated Bookmarks  After Delte On Display
+     fetchBookmarks();
 }
 
 // Fetch Bookmarks
@@ -65,6 +94,31 @@ function fetchBookmarks() {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
 
-        bookmarksResult.innerHTML += name;
+        bookmarksResult.innerHTML += "<div class='well'>"+
+                                     "<h3>"+name+"</h3>"+
+                                     " <a class='btn btn-default' target='_blank' href='"+url+"'>Visit</a> "+
+                                     " <a onclick='deleteBookmark(\""+url+"\")' class='btn btn-danger' href='#'>Delete</a>"+
+                                     "</h3>"+
+                                     "</div>";
     }
+}
+
+// Bookmarks Validation
+function validationBookmarks(siteName, siteUrl) {
+    // Empty Bookmarks Information Validation
+    if(!siteName || !siteUrl) {
+        alert("please Enter Required Informations");
+        return false;
+    }
+
+    // URL Validation
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if(!siteUrl.match(regex)) {
+        alert("Please Provide Valid Url");
+        return false;
+    }
+
+    return true;
 }
