18627cd7c60d4ac90a8712a7d31982fb488691b7
BaPpY <bappyasif@hotmail.com>
2019-12-23 02:21:05 +0600

    Added To Previous Commit With Rest Of This Custom Modal Example App, Along With Some Improvements And Extension ON Earlier Commit.

--- Custom Modal/main.js
+++ Custom Modal/main.js
@@ -3,12 +3,28 @@ var modal = document.getElementById("simpleModal");
 // Get Open Modal button
 var modalBtn = document.getElementById("modalBtn");
 // Get Close Button Access
-var closeBtn = document.getElementById("closeBtn");
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 // Lsiten For Click Event 
 modalBtn.addEventListener("click", openModal);
+// Listen For Close Modal
+closeBtn.addEventListener("click", closeModal);
+// Listen For Outside Click
+window.addEventListener("click", clickOutside);
 
 // Function To Open Modal
 function openModal() {
     modal.style.display = "block";
+}
+
+// Fucntion To Close Modal
+function closeModal(){
+    modal.style.display = "none";
+}
+
+// Fucntion To Close Modal When Clicked Outside Of Modal Box Area.
+function clickOutside(event){
+    if(event.target == modal) {
+        modal.style.display ="none";
+    }
 }
