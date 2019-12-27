2d86e8580d34711981c409f1e11b967a5f10638d
BaPpY <bappyasif@hotmail.com>
2019-12-28 02:03:37 +0600

    Added To Previous Commit With Rest Of This Text To Speech Sythensis Example App.

--- Text To Speech Synthesis Example App/dist/js/main.js
+++ Text To Speech Synthesis Example App/dist/js/main.js
@@ -9,7 +9,7 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
-//const body = document.querySelector('body');
+const body = document.querySelector('body');
 
 // init Voices Array
 let voices = [];
@@ -36,3 +36,71 @@ getVoices();
 if(synth.onvoiceschanged !== undefined) {
     synth.onvoiceschanged = getVoices;
 }
+
+// Speak Function
+const speak = () => {
+    // Check If It's Speaking
+    if(synth.speaking) {
+        console.error("Already Speaking...");
+        return;
+    }
+
+    if(textInput.value !== '') {
+        
+        // Adding Background Gif Animation
+        body.style.background = "#141414 url(dist/img/wave.gif)";
+        body.style.backgroundRepeat = "repeat-x" ;
+        body.style.backgroundSize = "100% 100%";
+
+        // Get Speak Input Text
+        const speakTest = new SpeechSynthesisUtterance(textInput.value);
+        
+        // Speak End
+        speakTest.onend = event => {
+            console.log("Done Speaking...");
+            // Chamging Background To Color
+            body.style.background = "#141414";
+        }
+
+        // Speak Error
+        speakTest.onerror = event => {
+            console.log("Something Wrong!!");
+        }
+
+        // Selected Voice
+        const selectedVoice = voiceSelect.selectedOptions[0]
+        .getAttribute("data-name");
+
+        // Loop Through Voices List
+        voices.forEach(voice => {
+            if(voice.name === selectedVoice) {
+                speakTest.voice = voice;
+            }
+        });
+
+        // Set Pitch And Rate
+        speakTest.rate = rate.value;
+        speakTest.pitch = pitch.value;
+
+        // Speak Text
+        synth.speak(speakTest);
+    }
+};
+
+// Event Listeners
+
+// Text Form Submit Type
+textForm.addEventListener("submit", event => {
+    event.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+// Rate Value Change Slider
+rate.addEventListener("change", event => rateValue.textContent = rate.value);
+
+// Pitch Value Change Slider
+pitch.addEventListener("change", event => pitchValue.textContent = pitch.value);
+
+// Voice Select Change Type
+voiceSelect.addEventListener("change", event => speak());
