bc0bfcc1c70d31d97f72cf237bab5908ab148cfe
BaPpY <bappyasif@hotmail.com>
2020-01-16 02:56:15 +0600

    Added To Previous Commit With Rest Of This Fancy Form Example App Build Up Which Is Powered By Node Saas.

--- Fancy Form Example App/js/main.js
+++ Fancy Form Example App/js/main.js
@@ -11,9 +11,9 @@
 // Questions Array
 const question_contents = [
     {question: "Enter Your First Name"},
-    {question: "Enter Your First Name"},
-    {question: "Enter Your First Name", pattern: /\S+@\S+\.\S+/},
-    {question: "Enter Your First Name", type: "password"}
+    {question: "Enter Your Last Name"},
+    {question: "Enter Your Email Address", pattern: /\S+@\S+\.\S+/},
+    {question: "Enter Your User Password", type: "password"}
 ];
 
 // Transition Times
@@ -34,7 +34,19 @@ const inputProgress = document.querySelector('#input-progress');
 const progressBar = document.querySelector('#progress-bar');
 
 // Events Listeners
+
+// Get Questions On Load
 document.addEventListener("DOMContentLoaded", getQuestions);
+
+// Next Button Clicked Event
+nextBtn.addEventListener("click", validateNext);
+
+// Input Field Enter Click
+inputField.addEventListener("keyup", event => {
+    if(event.keCode == 13) {
+        validateNext();
+    }
+});
 
 // Functions Definitions
 function getQuestions() {
@@ -48,7 +60,7 @@ function getQuestions() {
     inputField.focus();
 
     // Set Progress Bar Width - Variable To The Questions Length
-    progressBar.style.width = (question_position * 100) / (question_contents.length + "%");
+    progressBar.style.width = (question_position * 100) / question_contents.length + "%";
 
     // Add User Icon Or Back Arrow Deppending On Question
     prevBtn.className = question_position ? "fas fa-arrow-left" : "fas fa-user";
@@ -61,4 +73,81 @@ function showQuestions() {
     inputGroup.style.opacity = 1;
     inputProgress.style.transition = "";
     inputProgress.style.width = "100%";
+}
+
+//Hiding Questions From User
+function hideQuestion() {
+    inputGroup.style.opacity = 0;
+    inputLabel.style.marginLeft = 0;
+    inputProgress.style.width = 0;
+    inputProgress.style.transition = "none";
+    inputGroup.style.color = null;
+}
+
+// Transform To create Shake Motion Within Form
+function transformForm(x, y) {
+    //console.log(x,y);
+    formBox.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+// Validate Feild
+function validateNext() {
+    // Check If Pattern Matches
+    if(!inputField.value.match(question_contents[question_position].pattern || /.+/)) {
+        inputFail();
+    } else {
+        inputPass();
+    }
+}
+
+// Input Fail
+function inputFail() {
+    formBox.className = "error";
+    // Invoke Shake Motion For I Number Of Times
+    // Shaking On Sides Evenly.
+    for(let i=0; i<6; i++) {
+        setTimeout(transformForm, shakkeTime * i, ((i % 2) * 2 -1) * 20, 0);
+        setTimeout(transformForm, shakkeTime * 6, 0, 0);
+        inputField.focus();
+    }        
+}
+    
+// Input Pass
+function inputPass() {
+    formBox.className = "";
+    setTimeout(transformForm, shakkeTime * 0, 0, 10);
+    setTimeout(transformForm, shakkeTime * 1, 0, 0);
+
+    // Store Answers In Array
+    question_contents[question_position].answer = inputField.value;
+
+    // Increment Position
+    question_position++;
+
+    // When New Question, Hide Current And Get Next
+    if(question_contents[question_position]) {
+        hideQuestion();
+        getQuestions();
+    } else {
+        // Remov If No More Questions Exists
+        hideQuestion();
+        formBox.className = "close";
+        progressBar.style.width = "100%";
+
+        // Form Complete
+        formComplete();
+    }
+}
+
+// All Fields Complete - Show
+function formComplete() {
+    //console.log(question_contents);
+    const h1_element = document.createElement("h1");
+    h1_element.classList.add("end");
+    h1_element.appendChild(document.createTextNode(`Thanks ${question_contents[0].answer} 
+    You Are Registered And Will Recieve Notification Shortly`));
+    setTimeout(() => {
+        formBox.parentElement.appendChild(h1_element);
+        setTimeout(() => (h1_element.style.opacity = 1), 50); 
+    }, 1000);
 }
