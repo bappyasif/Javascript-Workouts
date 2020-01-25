a07673ca7e2782399c87aef76eb895c1d5a16a1a
BaPpY <bappyasif@hotmail.com>
2020-01-26 03:25:12 +0600

    Added To Previous Commit With Rest Of This Random Password Generator Example App Build Up.

--- Password Generator Example App/js/main.js
+++ Password Generator Example App/js/main.js
@@ -13,8 +13,9 @@ const randomPassword = {
     uppper_case: getRandomUpperLetters,
     real_numbers: getRandomRealNumbers,
     symbol_characters: getRandomSpecialSymbols
-};
+}
 
+// Event Listener For Generate
 generateElement.addEventListener('click', () => {
         //const length = lengthElement.value;
         const length = +lengthElement.value;
@@ -25,7 +26,70 @@ generateElement.addEventListener('click', () => {
 
         console.log(length, hasLower, hasUpper, hasNumber, hasSymbols);
         console.log(typeof length);
+
+        resultElement.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbols, length);
+
 });
+
+// Copy Password To Clipboad
+clipboardement.addEventListener("click", () => {
+    const text_area = document.createElement("textarea");
+    const password_created = resultElement.innerText;
+
+    if(!password_created) {
+        return;
+    }
+
+    text_area.value = password_created;
+    document.body.appendChild(text_area);
+    document.execCommand("copy");
+    text_area.remove();
+    alert("Password Copied To Clipbpard");
+});
+
+
+// Gnerate Password Function Definition
+function generatePassword(lower, upper, number, symol, length) {
+    // 1. Init Variable
+    let generated_password = "";
+    const typesCount = lower + upper + number + symol;
+    console.log(typesCount);
+    //const typesArray = [lower, upper, number, symol];
+    //const typesArray = [{lower}, {upper}, {number}, {symol}];
+
+    // 2. Filter Out Unchecked Types
+    const typesArray = [{lower}, {upper}, {number}, {symol}].filter(
+        item => Object.values(item)[0]
+    );
+    console.log(typesArray);
+
+    // 3. Lop Over Length Call Generator Function For Each Type
+    if(typesCount === 0) {
+        return "";
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            console.log("FuncName: "+funcName);
+            generated_password += randomPassword[funcName];
+        });
+    }
+
+    // for(let i=0; i<length; i+=typesCount) {
+	// 	typesArray.forEach(type => {
+    //         const funcName = Object.keys(type)[0];
+    //         console.log("FuncName: "+funcName);
+	// 		generated_password += randomPassword[funcName]();
+	// 	});
+	// }
+
+    // 4. Add Final Password To The Password Variable And Return.
+    console.log(generated_password.slice(0, length));
+    const final_password = generated_password.slice(0, length);
+
+    return final_password;
+}
 
 // Generator Functions
 function getRandomLowerLetters() {
